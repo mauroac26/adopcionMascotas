a9b4a14518d351ff5ec8f0f42699583efdc925a8
mauroac26 <mauroac26@gmail.com>
2022-09-07 07:52:19 -0300

    modificaciones

--- base/models.py
+++ base/models.py
@@ -9,7 +9,6 @@ class BaseModel(models.Model):
 
     # TODO: Define fields here
     id = models.AutoField(primary_key = True)
-    state = models.BooleanField('Estado',default = True)
     created_date = models.DateField('Fecha de Creación', auto_now=False, auto_now_add=True)
     modified_date = models.DateField('Fecha de Modificación', auto_now=True, auto_now_add=False)
     deleted_date = models.DateField('Fecha de Eliminación', auto_now=True, auto_now_add=False)
